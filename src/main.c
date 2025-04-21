c5244d66df8cddccacebe967a94690c8a2340eb6
Hugo <54435874+Cactooz@users.noreply.github.com>
2025-04-21 19:51:11 +0800

    Add invalid path checks for cmd line arguments

--- src/main.c
+++ src/main.c
@@ -9,27 +9,35 @@
 int main(int argc, char* argv[]) {
 	Image* image;
 	int name_len, modify_len, image_changed;
+	FILE* test_file;
 	char* save_name;
 	char* file_extension;
 	char* file_ending = "-modified.bmp";
 
 	if(argc < 2) {
-		fprintf(stderr, "Usage: %s <image_path> <output_path>\n", argv[0]);
+		fprintf(stderr, "Usage: %s image_path [output_path]\n", argv[0]);
 		fprintf(stderr, "\timage_path: path to a .bmp image file\n");
 		fprintf(stderr, "\toutput_path: path for the output .bmp image file (Optional)\n");
 		return 1;
 	}
 
-	image = read_image(argv[1]);
-	image_changed = menu(image);
-
-	if(!image_changed) {
-		printf("No changes were applied to the image.\n");
-		return 0;
+	test_file = fopen(argv[1], "r");
+	if(test_file == NULL) {
+		fprintf(stderr, "Error: Invalid input path '%s'\n", argv[1]);
+		fclose(test_file);
+		return 1;
 	}
+	fclose(test_file);
 
 	if(argc >= 3) {
 		save_name = argv[2];
+		test_file = fopen(save_name, "w");
+		if(test_file == NULL) {
+			fprintf(stderr, "Error: Invalid output path '%s'\n", save_name);
+			fclose(test_file);
+			return 1;
+		}
+		fclose(test_file);
 	} else {
 		file_extension = strrchr(argv[1], '.');
 		name_len = file_extension - argv[1];
@@ -39,6 +47,14 @@ int main(int argc, char* argv[]) {
 		save_name[name_len] = '\0';
 		strcat(save_name, file_ending);
 		save_name[name_len + modify_len] = '\0';
+	}
+
+	image = read_image(argv[1]);
+	image_changed = menu(image);
+
+	if(!image_changed) {
+		printf("No changes were applied to the image.\n");
+		return 0;
 	}
 
 	printf("Saving modified file to %s\n", save_name);
